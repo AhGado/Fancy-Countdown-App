5c7061858d56a71dd18326ebb88e2d9e78516f1b
Ahmed Gado <ahmedgado.dev@gmail.com>
2022-05-03 10:45:03 +0200

    Update

--- App Functionality.js
+++ App Functionality.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
   var initCount,
     counterVal = 0,
-    counting,
-    screenOn,
+    counting = false,
+    isPaused = false,
+    screenOn = false,
     interval,
+    lightMode = "Light",
     startBtn = $("#StartBtn"),
     stopBtn = $("#StopBtn"),
     onOffBtn = $("#OnOffBtn"),
@@ -20,11 +22,20 @@ $(document).ready(function () {
     var btnState = startBtn.attr("class");
     e.preventDefault();
     if (btnState === "Button") {
-      Check();
-      console.log("Checked!");
-      counting == true;
-      startBtn.toggleClass("Active");
-      stopBtn.toggleClass("Active");
+      counter.css("color", "rgb(255, 30, 30)");
+      if (isPaused && counting) {
+        isPaused = false;
+        counterVal = parseInt(counter.text());
+        startBtn.toggleClass("Active");
+        stopBtn.toggleClass("Active");
+        console.log("Continuing Counting..");
+      } else {
+        counting == true;
+        Check();
+        startBtn.toggleClass("Active");
+        stopBtn.toggleClass("Active");
+        console.log("Checked!");
+      }
     }
   });
 
@@ -33,10 +44,11 @@ $(document).ready(function () {
       startBtn.toggleClass("Active");
       stopBtn.toggleClass("Active");
       counter.css("color", "rgb(25, 25, 25)");
-      clearInterval(interval);
       counting == false;
+      isPaused = true;
       initCount = counterVal;
       $("#RotatingCircle").toggleClass("ON");
+      console.log("Paused!");
     }
   });
 
@@ -50,14 +62,15 @@ $(document).ready(function () {
   function OnOffSwitch() {
     clearInterval(interval);
     counting = false;
+    isPaused = false;
     $("#CounterText img").css("display", "none");
     $(".Switchable").toggleClass("ON");
     modeSwitch.toggleClass("Active");
     onOffBtn.toggleClass("Active");
+    counter.css("color", "rgb(25, 25, 25)");
+    console.log("image rdy")
+    console.log("Screen On!");
 
-    if (rotCircle.hasClass("ON")) {
-      rotCircle.toggleClass("ON");
-    }
     if (
       startBtn.hasClass("Active") &&
       stopBtn.hasClass("Active") &&
@@ -71,7 +84,11 @@ $(document).ready(function () {
 
     if (!stopBtn.hasClass("Active")) {
       stopBtn.toggleClass("Active");
-      counter.text(" ");
+      counter.text(" ")
+    }
+
+    if (rotCircle.hasClass("ON")) {
+      rotCircle.toggleClass("ON");
     }
   }
 
@@ -83,7 +100,6 @@ $(document).ready(function () {
       counterVal = initCount;
       console.log("value assigned");
       interval = setInterval(StartCount, 1000);
-      counter.css("color", "rgb(255, 30, 30)");
       $("#RotatingCircle").toggleClass("ON");
     } else {
       initCount = 10;
@@ -95,13 +111,17 @@ $(document).ready(function () {
   }
 
   function StartCount() {
+    console.log(isPaused);
+
     if (counterVal > 0) {
-      counting = true;
-      counterVal--;
-      counter.text(counterVal);
-      console.log(counterVal);
+      if (!isPaused) {
+        counting = true;
+        counterVal--;
+        counter.text(counterVal);
+        console.log(counterVal);
+      }
     } else {
-      console.log("counter value < 0");
+      console.log("Value < 0, or invalid");
       counting = false;
       clearInterval(interval);
       $("#RotatingCircle").toggleClass("ON");
